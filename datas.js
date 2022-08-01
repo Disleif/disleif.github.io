b0d99fa8a4848f48bbbfae90dfdf3e26116e087a
Disleif <58111527+Disleif@users.noreply.github.com>
2022-08-01 18:17:09 +0200

    Update datas.js

--- datas.js
+++ datas.js
@@ -19,14 +19,13 @@ const instructions = {
         { MrWhiteGuess: "Sa capacité entre en jeu ! Il peut tenter de deviner quel mot était le bon pour voler la victoire : "}, // entrée texte de l'utilisateur, passable en appuyant sur "skip"
         { eliminatedCivilian: " est éliminé. C'était un civil..."}, // message type "`nomJoueur` est éliminé..."
         { eliminatedUndercover: " est éliminé. C'était un undercover !"}, // playerName + eliminatedRole
-        { eliminatedMrWhite: " est éliminé. C'était Mister White !"},
+        { eliminatedMrWhite: " est éliminé. C'était Mr. White !"},
         { rules: "Voici le déroulement d'une partie :\nLes joueurs entrent à tour de rôle leur nom dans l'application. "+
                 "Suite à cela, les rôles sont désignés aléatoirement et gardés secret aux yeux des participants.\n"+
-                "Parmi ces rôles figurent :\nCivil : il connait son mot mais ne sait pas si il est civil ou undercover. Il doit éliminer tous les imposteurs\n"+
-                "Undercover : il connait son mot mais ne sait pas non plus s'il est civil ou undercover. Il doit survivre jusqu'à la fin\n"+
+                "Parmi ces rôles figurent :\nCivil : il connait son mot mais ne sait pas si il est civil ou undercover. Il doit éliminer tous les imposteurs.\n"+
+                "Undercover : il connait son mot mais ne sait pas non plus s'il est civil ou undercover. Il doit survivre jusqu'à la fin.\n"+
                 "Mr. White : il n'a pas de mot et doit déduire des autres le mot des civils tout en donnant des mots similaires pour ne pas éveiller les soupçons."+
-                " Il doit survivre jusqu'à la fin ou deviner le mot secret des civils\n\n"+
-                "Voici le déroulement d'une partie :\n"+
+                " Il doit survivre jusqu'à la fin ou deviner le mot secret des civils.\n\n"+
                 "Les joueurs donnent chacun leur tour un mot en rapport avec celui qui leur a été donné, il faut cependant rester vague car si l'on"+
                 " est civil il faut éviter de donner des informations à Mr. White et si l'on est undercover il ne faut pas se faire attraper...\n"+
                 "Mr. White quant à lui, n'ayant pas de mot, déduit de ceux énoncés au préalable le thème autour duquel tournent les mots en jeu. Il doit alors"+
@@ -51,5 +50,20 @@ const instructions = {
         { eliminatedCivilian: " is eliminated. C'était un civil..."},
         { eliminatedUndercover: " is eliminated. C'était un undercover !"},
         { eliminatedMrWhite: " is eliminated. C'était Mister White !"},
+        { rules: "Here is how a game goes:\nPlayers start by typing their name on the application. "+
+                "Roles are then randomly assigned to each player, kept secret from them.\n"+
+                "The existing roles are as follows:\nCivilian: he knows his word but doesn't know whether he is a civilian or an undercover. His job is to eliminate all impostors\n"+
+                "Undercover: Same as the civilian. His goal is to survive.\n"+
+                "Mr. White: He doesn't starts with a word and has to deduce the civilian's secret word while giving similar words to not be discovered."+
+                " He can win by either surviving or once eliminated: guessing the civilian's word right.\n\n"+
+                "Players have to give a word, turn by turn, that is related to the one they were given while staying vague.\n"+
+                "Indeed, if civilian: informations need to be the smallest possible so Mr. White can't easily guess and if undercover: staying incognito is key...\n"+
+                "Mr. White has a special role as he start without any word. He thus needs to deduce from others's words the theme around the game plays. "+
+                "He then has to give words close enough to not get spotted by the other players.\n\n"+
+                "Whatever the role, players need to protect their word while guessing the other teams's word, try to understand which players are allies or enemies and find Mr. White.\n\n"+
+                "After the first 2 rounds, players debate and proceed to a vote to eliminate a player. Each player points at another and counts are made.\n"+
+                "The designated player is then eliminated. If he was Mr. White, he can try to steal the victory by guessing to civilians's secret word.\n"+
+                "Once this is done, if the application doesn't declare a winner, the game continues:"+
+                " another round is played with players giving words followed by another vote, repeated until a team wins the game."}
     ],
 };
